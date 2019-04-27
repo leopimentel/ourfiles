342f4aa12fa6118ecaaa1eea6ae8fa9535b5cc9b
Leonardo P Ferreira <lpfw82@gmail.com>
2019-04-27 17:05:37 -0300

    Estrutura backend

---
+++ src/models/File.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const File = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    path: {
+        type: String,
+        required: true
+    },
+    files: []
+}, {
+    timestamps: true,
+    toObject: {virtuals: true},//fala pra calcular propriedades virtuais como a url abaixo
+    toJSON: {virtuals: true},
+});
+
+//campo q n ta no BD 
+File.virtual('url').get(function() {
+    return `http://localhost:3333/files/${encodeURIComponent(this.path)}`;
+});
+
+module.exports = mongoose.model("File", File);
---
+++ src/server.js
@@ -0,0 +1,55 @@
+//express - define rotas e outras coisas pra o app
+const express = require('express');
+
+//ORM - escrever em js 
+const mongoose = require('mongoose');
+
+const server = require('http').Server(app);
+
+//aqi app ja entende http e socket
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+    console.log('ok');
+    socket.on('connectRoom', box => {
+        socket.join(box);
+    });
+});
+
+//uom4Z#g5*Kn
+mongoose.connect('mongodb+srv://omnistack:jo0KC0lLR0t387sk@cursonode-ijeke.mongodb.net/omnistack?retryWrites=true',
+{
+    useNewUrlParser: true,   
+});
+
+//nodemon = hotreload do server
+
+//salva variaveis da app
+const app = express();
+
+app.use(cors());
+
+const path = require('path');
+
+const cors = require('cors');
+
+//aceita json
+app.use(express.json());
+
+
+//aceita arquivo
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
+
+//importa as rotas
+app.use(require('./routes'));
+
+//middleware
+app.use((req, res, next) => {
+    req.io = io;
+    //precisa passa para o next middleware
+    return next();
+});
+
+server.listen(3333);
